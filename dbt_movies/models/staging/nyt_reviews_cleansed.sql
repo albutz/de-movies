a29cec929bba72f145faf6b3dad3e590d9b6a342
Alexander Butz <alex.butz@gmail.com>
2022-10-27 22:32:33 +0200

    feat(nyt_reviews_cleansed): update nyt_reviews_cleansed incrementally

--- dbt_movies/models/staging/nyt_reviews_cleansed.sql
+++ dbt_movies/models/staging/nyt_reviews_cleansed.sql
@@ -1,21 +1,37 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
 WITH nyt_reviews AS (
     SELECT * FROM {{ source('movies', 'nyt') }}
 )
 
 SELECT
-    $1:byline::STRING AS author_name,
-    $1:critics_pick::INTEGER::BOOLEAN AS is_critics_pick,
-    $1:date_updated::DATE AS review_date_updated,
-    $1:display_title::STRING AS movie_title,
-    $1:headline::STRING AS review_headline,
-    $1:link.url::STRING AS review_url,
-    CASE 
-        WHEN $1:mpaa_rating::STRING = '' THEN NULL
-        ELSE $1:mpaa_rating::STRING
-    END AS mpaa_rating,
-    $1:multimedia.src::STRING AS image_url,
-    $1:opening_date::DATE AS opening_date,
-    $1:publication_date::DATE AS review_date,
-    $1:summary_short::STRING AS summary_short
-FROM
-    nyt_reviews
+    {{ dbt_utils.surrogate_key(['author_name', 'movie_title', 'review_headline']) }} AS id,
+    *
+FROM (
+    SELECT
+        $1:byline::STRING AS author_name,
+        $1:critics_pick::INTEGER::BOOLEAN AS is_critics_pick,
+        $1:date_updated::DATE AS review_date_updated,
+        $1:display_title::STRING AS movie_title,
+        $1:headline::STRING AS review_headline,
+        $1:link.url::STRING AS review_url,
+        CASE 
+            WHEN $1:mpaa_rating::STRING = '' THEN NULL
+            ELSE $1:mpaa_rating::STRING
+        END AS mpaa_rating,
+        $1:multimedia.src::STRING AS image_url,
+        $1:opening_date::DATE AS opening_date,
+        $1:publication_date::DATE AS review_date,
+        $1:summary_short::STRING AS summary_short
+    FROM
+        nyt_reviews
+) 
+{% if is_incremental() %}
+WHERE 
+    review_date_updated > (SELECT MAX(review_date_updated) FROM {{ this }})
+{% endif %}

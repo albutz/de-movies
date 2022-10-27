7620ed684db98394bfb811b278972311a196e07f
Alexander Butz <alex.butz@gmail.com>
2022-10-28 01:26:10 +0200

    feat(imdb_ratings_cleansed): update imdb_ratings_cleansed incrementally

--- dbt_movies/models/staging/imdb_ratings_cleansed.sql
+++ dbt_movies/models/staging/imdb_ratings_cleansed.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='id'
+    )
+}}
+
 WITH imdb_ratings AS (
     SELECT *
     FROM {{ source('movies', 'imdb_ratings') }}
@@ -9,9 +16,16 @@ imdb_basics AS (
 
 SELECT
     r.id AS id,
+    CURRENT_TIMESTAMP() AS updated_at,
     average_rating,
     num_votes
 FROM
     imdb_ratings r
     JOIN imdb_basics b 
     ON r.id = b.id
+-- The updated_at field will be updated with every run, but all other fields
+-- and new records will be updated / inserted incrementally.
+{% if is_incremental() %}
+WHERE
+    updated_at > (SELECT MAX(updated_at) FROM {{ this }})
+{% endif %}

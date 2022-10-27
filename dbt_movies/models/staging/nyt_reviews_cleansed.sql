26657bb92cb7c5086cf94251a54f8f527afe13b2
Alexander Butz <alex.butz@gmail.com>
2022-10-27 17:57:28 +0200

    feat(nyt_reviews_cleansed.sql): add model to stage nyt reviews

---
+++ dbt_movies/models/staging/nyt_reviews_cleansed.sql
@@ -0,0 +1,18 @@
+WITH nyt_reviews AS (
+    SELECT * FROM {{ source('movies', 'nyt') }}
+)
+
+SELECT
+    $1:byline::STRING AS author_name,
+    $1:critics_pick::INTEGER::BOOLEAN AS is_critics_pick,
+    $1:date_updated::DATE AS review_date_updated,
+    $1:display_title::STRING AS movie_title,
+    $1:headline::STRING AS review_headline,
+    $1:link.url::STRING AS review_url,
+    $1:mpaa_rating::STRING AS mpaa_rating,
+    $1:multimedia.src::STRING AS image_url,
+    $1:opening_date::DATE AS opening_date,
+    $1:publication_date::DATE AS review_date,
+    $1:summary_short::STRING AS summary_short
+FROM
+    nyt_reviews
